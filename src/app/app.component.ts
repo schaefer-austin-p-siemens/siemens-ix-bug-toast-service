ccc0ecec1bd6d28c82cedb0252efb187e3c64f8c
Austin Schaefer <schaefer.austin@siemens.com>
2025-07-21 11:13:43 +0100

    Display toastService bug in Firefox, Chrome.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component, inject
+} from '@angular/core';
+import {
+  IxButton,
+  ToastService
+} from '@siemens/ix-angular/standalone';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IxButton],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'siemens-ix-bug-toast-service';
+
+  private readonly toastService = inject(ToastService);
+
+  createToast() {
+  }
 }
